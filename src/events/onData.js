6f718b1ff6740769120f026e586c2f137565e355
rlaguswls2 <hydrated200ml@gmail.com>
2024-11-05 20:48:21 +0900

    login test

--- src/events/onData.js
+++ src/events/onData.js
@@ -1,5 +1,11 @@
-import { PACKET_TYPE, PACKET_TYPE_LENGTH, TOTAL_LENGTH } from '../constants/header.js';
-import { getHandlerById } from '../handler/index.js';
+import {
+  PACKET_TYPE,
+  PACKET_TYPE_LENGTH,
+  PAYLOAD_LENGTH_SIZE,
+  TOTAL_LENGTH,
+  VERSION_START,
+} from '../constants/header.js';
+import { getHandlerByPacketType } from '../handler/index.js';
 import { packetParser } from '../utils/parser/packetParser.js';
 
 export const onData = (socket) => async (data) => {
@@ -13,43 +19,29 @@ export const onData = (socket) => async (data) => {
     const versionLength = socket.buffer.readUInt8(2);
     console.log(`versionLength: ${versionLength}`);
 
-    const versionStart = 3;
-    const version = socket.buffer.toString('utf8', versionStart, versionStart + versionLength);
+    const version = socket.buffer.toString('utf8', VERSION_START, VERSION_START + versionLength);
     console.log(`version: ${version}`);
 
     // sequence: 4바이트 읽기
-    const sequence = socket.buffer.readUInt32BE(versionStart + versionLength);
+    const sequence = socket.buffer.readUInt32BE(VERSION_START + versionLength);
     console.log(`sequence: ${sequence}`);
 
     // payloadLength: 4바이트 읽기
-    const payloadLengthPosition = versionStart + versionLength + 4;
+    const payloadLengthPosition = VERSION_START + versionLength + PAYLOAD_LENGTH_SIZE;
     const payloadLength = socket.buffer.readUInt32BE(payloadLengthPosition);
     console.log(`payloadLength: ${payloadLength}`);
 
     // 패킷의 전체 길이 (헤더와 payload 길이를 포함)
-    const length = payloadLengthPosition + 4 + payloadLength;
-    console.log('socket.buffer end');
+    const length = payloadLengthPosition + PAYLOAD_LENGTH_SIZE + payloadLength;
     if (socket.buffer.length >= length) {
       // 헤더부터 끝까지
-      const payloadStart = payloadLengthPosition + 4;
-      const payload = socket.buffer.subarray(payloadStart, payloadStart + payloadLength);
-      console.log(payload); // 데이터가 문자열이라 가정하고 출력
+      const payloadStart = payloadLengthPosition + PAYLOAD_LENGTH_SIZE;
+      let payload = socket.buffer.subarray(payloadStart, payloadStart + payloadLength);
 
-      const packet = socket.buffer.subarray(totalHeaderLength, length);
       socket.buffer = socket.buffer.subarray(length);
       try {
-        switch (packetType) {
-          case PACKET_TYPE.REGISTER_REQUEST: {
-            //console.log(payload);
-            packetParser(packet);
-            //const { handlerId, userId, payload } = packetParser(packet);
-            //console.log(payload.x, payload.y);
-            // userId가 존재하는지 확인하고 없으면 추가
-
-            //const handler = getHandlerById(handlerId);
-            //handler({ socket, userId, payload });
-          }
-        }
+        const handler = getHandlerByPacketType(packetType);
+        handler({ socket, payload });
       } catch (error) {
         console.error(error);
       }
