2667c889c99fb6ce7dba1b848d41f40c9672c8a2
rlaguswls2 <hydrated200ml@gmail.com>
2024-11-05 14:34:07 +0900

    패킷 연결 확인테스트

--- src/events/onConnection.js
+++ src/events/onConnection.js
@@ -6,7 +6,6 @@ export const onConnection = async (socket) => {
   console.log(`Client connected from: ${socket.remoteAddress}`);
 
   socket.buffer = Buffer.alloc(0);
-
   socket.on('data', onData(socket));
   socket.on('end', onEnd(socket));
   socket.on('error', onError(socket));
--- src/utils/parser/packetParser.js
+++ src/utils/parser/packetParser.js
@@ -2,47 +2,49 @@ import { CLIENT_VERSION } from '../../constants/env.js';
 import { getProtoTypeNameByHandlerId } from '../../handler/index.js';
 import { getProtoMessages } from '../../init/loadProto.js';
 
-export const packetParser = (data) => {
+export const packetParser = (payload) => {
   const protoMessages = getProtoMessages();
 
-  const Packet = protoMessages.common.Packet;
-
+  const Packet = protoMessages.test.C2SRegisterRequest;
+  console.log(`in packetParser.js Packet: ${Packet}`);
   let packet;
+  console.log(`in packetParser.js data: ${payload}`);
   try {
-    packet = Packet.decode(data);
+    packet = Packet.decode(payload);
+    console.log(`in packetParser.js packet: ${packet}`);
   } catch (error) {
     console.error(error);
   }
 
-  const handlerId = packet.handlerId;
-  const userId = packet.userId;
-  const clientVersion = packet.version;
+  // const handlerId = packet.handlerId;
+  // const userId = packet.userId;
+  // const clientVersion = packet.version;
 
-  if (clientVersion !== CLIENT_VERSION) {
-    throw Error();
-  }
-  const protoTypeName = getProtoTypeNameByHandlerId(handlerId);
-  if (!protoTypeName) {
-    throw Error();
-  }
+  // if (clientVersion !== CLIENT_VERSION) {
+  //   throw Error();
+  // }
+  // const protoTypeName = getProtoTypeNameByHandlerId(handlerId);
+  // if (!protoTypeName) {
+  //   throw Error();
+  // }
 
-  const [namespace, typeName] = protoTypeName.split('.');
-  const payloadType = protoMessages[namespace][typeName];
-  let payload;
+  // const [namespace, typeName] = protoTypeName.split('.');
+  // const payloadType = protoMessages[namespace][typeName];
+  // let payload;
 
-  try {
-    payload = payloadType.decode(packet.payload);
-  } catch (error) {
-    console.error(error);
-  }
+  // try {
+  //   payload = payloadType.decode(packet.payload);
+  // } catch (error) {
+  //   console.error(error);
+  // }
 
-  const expectedFields = Object.keys(payloadType.fields);
-  const actualFields = Object.keys(payload);
-  const missingFields = expectedFields.filter((field) => !actualFields.includes(field));
+  // const expectedFields = Object.keys(payloadType.fields);
+  // const actualFields = Object.keys(payload);
+  // const missingFields = expectedFields.filter((field) => !actualFields.includes(field));
 
-  if (missingFields > 0) {
-    throw Error();
-  }
+  // if (missingFields > 0) {
+  //   throw Error();
+  // }
 
-  return { handlerId, userId, payload };
+  // return { handlerId, userId, payload };
 };
