98aa1ae58548be4dd8003c50f2fe64c0fe0b6b8f
UIHyeonLEE <zlzmfdkdl@naver.com>
2024-11-07 19:23:54 +0900

    commit

--- src/handler/game/monsterAttackBase.handler.js
+++ src/handler/game/monsterAttackBase.handler.js
@@ -8,7 +8,7 @@ let baseState = {
 };
 
 // 몬스터 공격 요청 처리 핸들러
-const handleMonsterAttackRequest = (socket, packet) => {
+const handleMonsterAttackRequest = ({ socket, packet }) => {
   try {
     const { damage } = packet;
 
@@ -25,6 +25,7 @@ const handleMonsterAttackRequest = (socket, packet) => {
 // 소켓 연결 시 클라이언트로부터 오는 메시지 리스너
 const setupSocketListeners = (socket) => {
   console.log('소켓 객체:', socket);
+
   socket.on(PACKET_TYPE.MONSTER_ATTACK_BASE_REQUEST, (packet) => {
     handleMonsterAttackRequest(socket, packet);
   });
