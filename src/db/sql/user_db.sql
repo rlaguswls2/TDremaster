e5a67a8d5de8873ec31e5f59f6d9b1f20db8898e
adfio1234 <dlwlsdnr5246@gmail.com>
2024-11-07 16:07:10 +0900

    user테이블 재정의 및 register에서 jwt부분과 passwordconfirm제거

--- src/db/sql/user_db.sql
+++ src/db/sql/user_db.sql
@@ -1,8 +1,9 @@
 CREATE TABLE IF NOT EXISTS `user`
 (
-  device_id VARCHAR(255) PRIMARY KEY,
-  x_coord DOUBLE NOT NULL DEFAULT 0,
-  y_coord DOUBLE NOT NULL DEFAULT 0,
+  user_id       INT           PRIMARY KEY AUTO_INCREMENT,
+  password      VARCHAR(255)  NOT NULL,
+  email         VARCHAR(255)  NOT NULL,
+  highest_score INT           DEFAULT 0,
   last_login TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
--- src/handler/auth/register.handler.js
+++ src/handler/auth/register.handler.js
@@ -10,7 +10,7 @@ const register = ({ socket, payload }) => {
     const protoMessages = getProtoMessages();
 
     const GamePacket = protoMessages.test.GamePacket;
-    const gamePacket = GamePacket.decode(payload);
+    const gamePacket = GamePacket.decode(payload);//decode해준다.
     console.log(`Decoded GamePacket:`, gamePacket);
 
     const registerRequest = gamePacket.registerRequest;//1번 register요청 /src/protobuf/test.proto
@@ -18,21 +18,21 @@ const register = ({ socket, payload }) => {
       throw new Error('Invalid payload type in GamePacket for register request.');
     }
 
-    const { email,id, password,passwordConfirm } = registerRequest;//email,id,password,passwordconfirm을 입력받는다.
-    
+
+
+    // const { email, id, password, passwordConfirm } = registerRequest;
+    const { email, id, password} = registerRequest;//email,id,password을 입력받는다. passwordConfirm을 일단 제외한다.
+    console.log(`in registerHandler.js data: ${email}, ${id}, ${password}`);
+
     //비밀번호 비밀번호 확인이 일치하는지 확인
-    if(password!==passwordConfirm){
-        throw new Error('Dismatch password passwordConfirm.');
-    }
+    // if (password !== passwordConfirm) {
+    //   throw new Error('Dismatch password passwordConfirm.');
+    // }
     //값이 제대로 들어갔는지 확인
-    console.log(`in registerHandler.js data: ${email}, ${id}, ${password} ${passwordConfirm}`);
-
-
-    // 회원가입 로직 처리 (예: ID와 비밀번호 검증)
-    // const success = true; // 회원가입 성공 여부를 예시로 설정
-    // const message = success ? 'Register successful.' : 'Register failed.';
-    const jwtToken=jwt.sign({email,id},"SECRET_KEY",{ expiresIn: '1h' });//SECRET_KEY부분임시로 채움, 만료시간 1시간으로 설정
     
+    //회원가입시 jwt발급이 필요한지 고민
+    // const jwtToken = jwt.sign({ email, id }, "SECRET_KEY", { expiresIn: '1h' });//SECRET_KEY부분임시로 채움, 만료시간 1시간으로 설정
+
     //db에 회원가입 정보 채울 공간
 
 
@@ -42,30 +42,28 @@ const register = ({ socket, payload }) => {
 
 
 
-    // const token = success ? 'some-generated-token' : ''; // 로그인 성공 시 토큰 생성
-    // const failCode = success
-    //   ? protoMessages.test.GlobalFailCode.NONE
-    //   : protoMessages.common.GlobalFailCode.AUTHENTICATION_FAILED;
-
     // S2CRegisterResponse 메시지 생성 및 직렬화
     const S2CRegisterResponse = protoMessages.test.S2CRegisterResponse;
-    const responsePayload = S2CRegisterResponse.create({ 
-      id:id,
-      password:password,
-      email:email, });//
+    const responsePayload = S2CRegisterResponse.create({
+      id: id,
+      password: password,
+      email: email,
+    });//
 
     sendResponsePacket(socket, PACKET_TYPE.REGISTER_RESPONSE, {
       registerResponse: responsePayload,
     });
+    
   } catch (error) {
     //register실패시 
     console.error('Error handling register request:', error);
 
     const S2CRegisterResponse = protoMessages.test.S2CRegisterResponse;
-    const responsePayload = S2CRegisterResponse.create({ 
-      id:'',
-      password:'',
-      email:'', });// 실패시 넘겨줄 response
+    const responsePayload = S2CRegisterResponse.create({
+      id: '',
+      password: '',
+      email: '',
+    });// 실패시 넘겨줄 response
 
     sendResponsePacket(socket, PACKET_TYPE.REGISTER_RESPONSE, {
       registerResponse: responsePayload,
