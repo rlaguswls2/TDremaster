eeea73d1240d6f312d709c476910b0aaa21d4b6a
UIHyeonLEE <zlzmfdkdl@naver.com>
2024-11-08 17:37:07 +0900

    로그인 주석

--- src/handler/auth/login.handler.js
+++ src/handler/auth/login.handler.js
@@ -4,7 +4,7 @@ import { getProtoMessages } from '../../init/loadProto.js';
 import { getGameSession } from '../../sessions/game.session.js';
 import sendResponsePacket from '../../utils/response/createResponse.js';
 import { serializer } from '../../utils/serializer.js';
-import jwt from 'jsonwebtoken';//jwt토큰 발급을 위한 jwt 임포트
+import jwt from 'jsonwebtoken'; //jwt토큰 발급을 위한 jwt 임포트
 import bcrypt from 'bcrypt';
 const login = async ({ socket, payload }) => {
   try {
@@ -19,29 +19,30 @@ const login = async ({ socket, payload }) => {
       throw new Error('Invalid payload type in GamePacket for login request.');
     }
 
-    const { id, password } = loginRequest;//id와 password를 받아온다.
+    const { id, password } = loginRequest; //id와 password를 받아온다.
     console.log(`in loginHandler.js data: ${id}, ${password}`);
-    
 
     //user가 존재하는지 확인
-    const success = true;// 로그인 성공 여부를 예시로 설정
-    const existuser=await findUserById(id);
-    
+    const success = true; // 로그인 성공 여부를 예시로 설정
+
     // id와 password로 인증 로직 처리
-    
-    if(existuser===null)//id가 일치하는지 확인
-    {
-      console.log("Player is not exist!");
-      success=false;
-    }
-    if(!(await bcrypt.compare(password, existuser.password)))//비밀번호가 일치하는지 확인
-    {
-      console.log("pass word is dismatch!");
-      success=false;
-    }
+
+    // 나중에 풀어야함=========================================================================================================================
+    //const existuser = await findUserById(id);
+    // if(existuser===null)//id가 일치하는지 확인
+    // {
+    //   console.log("Player is not exist!");
+    //   success=false;
+    // }
+    // if(!(await bcrypt.compare(password, existuser.password)))//비밀번호가 일치하는지 확인
+    // {
+    //   console.log("pass word is dismatch!");
+    //   success=false;
+    // }
+
     // 로그인 로직 처리 (예: ID와 비밀번호 검증)
     const message = success ? 'Login successful.' : 'Login failed.';
-    const jwtToken = jwt.sign({ id, password }, "SECRET_KEY", { expiresIn: '1h' });//SECRET_KEY부분임시로 채움, 만료시간 1시간으로 설정
+    const jwtToken = jwt.sign({ id, password }, 'SECRET_KEY', { expiresIn: '1h' }); //SECRET_KEY부분임시로 채움, 만료시간 1시간으로 설정
     const failCode = success
       ? protoMessages.test.GlobalFailCode.NONE
       : protoMessages.common.GlobalFailCode.AUTHENTICATION_FAILED;
