bf075d2809075f2430739922dc1604fb65dcece8
applaud13 <applaud13@gmail.com>
2024-11-09 00:09:08 +0900

    연결 종료 시 함수 처리
    게임 중인 유저가 강제로 종료 시
    관리중이던 세션의 데이터들 삭제 및 상대방 플레이어에게 승리 함수 보내기

--- src/events/onEnd.js
+++ src/events/onEnd.js
@@ -1,3 +1,17 @@
+import S2CGameOverNotification from '../handler/game/gameOver.handler.js';
+import { removePlayerState } from '../sessions/game.session.js';
+import { activePlayers, playerState } from '../sessions/sessions.js';
+import { clearMatch, getOpponentSocket } from '../sessions/user.session.js';
+
 export const onEnd = (socket) => async () => {
   console.log('클라이언트 연결이 종료되었습니다.');
+  console.log('삭제 전 남은 플레이어 수: ', playerState.length);
+  console.log('삭제 전 남은 매칭게임 수: ', activePlayers.length);
+  const opponentSocket = getOpponentSocket(socket);
+  S2CGameOverNotification({ socket: opponentSocket, isWin: true });
+  removePlayerState(socket);
+  removePlayerState(opponentSocket);
+  clearMatch(socket);
+  console.log('삭제 후 남은 플레이어 수: ', playerState.length);
+  console.log('삭제 후 남은 매칭게임 수: ', activePlayers.length);
 };
