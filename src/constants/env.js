90b36181b9debeffbab9cc008bf0a5e0c236cc37
adfio1234 <dlwlsdnr5246@gmail.com>
2024-11-08 17:03:58 +0900

    .env로 secretket관리

--- src/constants/env.js
+++ src/constants/env.js
@@ -11,3 +11,4 @@ export const DB_USER = process.env.DB_USER || 'root';
 export const DB_PASSWORD = process.env.DB_PASSWORD || 'jstk350!';
 export const DB_HOST = process.env.DB_HOST || '127.0.0.1';
 export const DB_PORT = process.env.DB_PORT || '3307';
+export const SECRET_KEY=process.env.SECRET_KEY||'SECRET_KEY';
