b8608652907cd83d3ca49d7fb8b3e3922e887cb0
UIHyeonLEE <zlzmfdkdl@naver.com>
2024-11-08 16:54:36 +0900

    데미지 추가

--- src/handler/game/monsterAttackBase.handler.js
+++ src/handler/game/monsterAttackBase.handler.js
@@ -5,15 +5,18 @@ import sendResponsePacket from '../../utils/response/createResponse.js';
 // 몬스터 공격 요청 처리 핸들러
 const monsterAttackBaseHandler = ({ payload }) => {
   try {
-    const damage = payload.damage; // 클라이언트에서 보내는 damage
+    const protoMessages = getProtoMessages();
+
+    const GamePacket = protoMessages.test.GamePacket;
+    const gamePacket = GamePacket.decode(payload);
+    console.log(`Decoded GamePacket:`, gamePacket);
+
+    const damage = gamePacket.monsterAttackBaseRequest;
 
     if (typeof damage === 'undefined') {
       console.error('damage가 없습니다:', payload);
       return;
     }
-
-    // 클라이언트에 기지 HP 업데이트 알림 전송
-    const protoMessages = getProtoMessages();
 
     if (!protoMessages || !protoMessages.test) {
       console.error('ProtoBuf 메시지가 올바르게 로드되지 않았습니다.');
